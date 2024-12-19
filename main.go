345122cc899516b91e2f1b9178698b96f7287885
Larrry85 <levisto.laura@gmail.com>
2024-12-19 16:57:01 +0200

    corrected paths

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func main() {
 	http.HandleFunc("/", rootHandler)
 
 	// Serve CSS and JavaScript files from /home/larrry/laura2/
-	http.Handle("/testapp/styles/", http.StripPrefix("/testapp/styles/", http.FileServer(http.Dir("testapp/styles"))))
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles"))))
 	// Serve JavaScript file
-	http.Handle("/testapp/scripts/", http.StripPrefix("/testapp/scripts/", http.FileServer(http.Dir("testapp/scripts"))))
+	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
 	/*
 	   http.HandleFunc("/scripts/script.js", func(w http.ResponseWriter, r *http.Request) {
 	       w.Header().Set("Content-Type", "application/javascript")
@@ -25,5 +25,5 @@ func main() {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "testapp/index.html")
+	http.ServeFile(w, r, "index.html")
 }
